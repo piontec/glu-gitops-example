373902e21fb6e104a729259d0787cb079cfcc11a
George MacRorie <me@georgemac.com>
2024-11-20 13:36:23 +0000

    feat: add gitops example glu pipeline

---
+++ cmd/pipeline/go.mod
@@ -0,0 +1,68 @@
+module get-glu/gitops-example/pipeline
+
+go 1.23.3
+
+require (
+	github.com/get-glu/glu v0.0.1-prerelease2
+	github.com/get-glu/glu/ui v0.0.1-prerelease2
+	github.com/opencontainers/image-spec v1.1.0
+	k8s.io/api v0.31.2
+	k8s.io/apimachinery v0.31.2
+	oras.land/oras-go/v2 v2.5.0
+	sigs.k8s.io/yaml v1.4.0
+)
+
+require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.1.2 // indirect
+	github.com/bradleyfalzon/ghinstallation/v2 v2.12.0 // indirect
+	github.com/cloudflare/circl v1.5.0 // indirect
+	github.com/cyphar/filepath-securejoin v0.3.4 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.0 // indirect
+	github.com/go-git/go-git/v5 v5.12.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/go-github/v64 v64.0.0 // indirect
+	github.com/google/go-github/v66 v66.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pjbgf/sha1cd v0.3.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.3.0 // indirect
+	github.com/whilp/git-urls v1.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+)
+
+replace github.com/get-glu/glu => ../../../../flipt-io/glu
+
+replace github.com/get-glu/glu/ui => ../../../../flipt-io/glu/ui
---
+++ cmd/pipeline/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/get-glu/glu"
+	"github.com/get-glu/glu/pkg/core"
+	"github.com/get-glu/glu/pkg/fs"
+	"github.com/get-glu/glu/pkg/phases"
+	"github.com/get-glu/glu/pkg/src/git"
+	"github.com/get-glu/glu/pkg/src/oci"
+	"github.com/get-glu/glu/ui"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	k8syaml "k8s.io/apimachinery/pkg/util/yaml"
+	"oras.land/oras-go/v2/registry"
+	"sigs.k8s.io/yaml"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil && !errors.Is(err, context.Canceled) {
+		slog.Error("system exiting", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	return glu.NewSystem(ctx, glu.Name("gitops-example"), glu.WithUI(ui.FS())).
+		AddPipeline(func(ctx context.Context, config *glu.Config) (glu.Pipeline, error) {
+			// fetch the configured OCI repositority source named "checkout"
+			ociRepo, err := config.OCIRepository("app")
+			if err != nil {
+				return nil, err
+			}
+
+			ociSource := oci.New[*AppResource](ociRepo)
+
+			// fetch the configured Git repository source named "checkout"
+			gitRepo, gitProposer, err := config.GitRepository(ctx, "gitopsexample")
+			if err != nil {
+				return nil, err
+			}
+
+			gitSource := git.NewSource[*AppResource](gitRepo, gitProposer)
+
+			// create initial (empty) pipeline
+			pipeline := glu.NewPipeline(glu.Name("gitops-example-app"), func() *AppResource {
+				return &AppResource{
+					Image: "ghcr.io/get-glu/gitops-example/app",
+				}
+			})
+
+			// build a phase which sources from the OCI repository
+			ociPhase, err := phases.New(glu.Name("oci"), pipeline, ociSource)
+			if err != nil {
+				return nil, err
+			}
+
+			// build a phase for the staging environment which source from the git repository
+			// configure it to promote from the OCI phase
+			staging, err := phases.New(glu.Name("staging"),
+				pipeline, gitSource, core.PromotesFrom(ociPhase))
+			if err != nil {
+				return nil, err
+			}
+
+			// build a phase for the production environment which source from the git repository
+			// configure it to promote from the staging git phase
+			_, err = phases.New(glu.Name("production"),
+				pipeline, gitSource, core.PromotesFrom(staging))
+			if err != nil {
+				return nil, err
+			}
+
+			// return configured pipeline to the system
+			return pipeline, nil
+		}).
+		//AddTrigger(
+		//	schedule.New(
+		//		schedule.WithInterval(10*time.Second),
+		//		schedule.MatchesLabel("env", "staging"),
+		//		// alternatively, the phase instance can be target directly with:
+		//		// glu.ScheduleMatchesPhase(gitStaging),
+		//	),
+		//).
+		Run()
+}
+
+// AppResource is a custom envelope for carrying our specific repository configuration
+// from one source to the next in our pipeline.
+type AppResource struct {
+	Image       string
+	ImageDigest string
+}
+
+// Digest is a core required function for implementing glu.Resource
+// It should return a unique digest for the state of the resource.
+// In this instance we happen to be reading a unique digest from the source
+// and so we can lean into that.
+// This will be used for comparisons in the phase to decided whether or not
+// a change has occurred when deciding if to update the target source.
+func (c *AppResource) Digest() (string, error) {
+	return c.ImageDigest, nil
+}
+
+// ReadFromOCIDescriptor is an OCI specific resource requirement.
+// Its purpose is to read the resources state from a target OCI metadata descriptor.
+// Here we're reading out the images digest from the metadata.
+func (c *AppResource) ReadFromOCIDescriptor(d v1.Descriptor) error {
+	c.ImageDigest = d.Digest.String()
+	return nil
+}
+
+// ReadFrom is a Git specific resource requirement.
+// It specifies how to read the resource from a target Filesystem.
+// The type should navigate and source the relevant state from the fileystem provided.
+// The function is also provided with metadata for the calling phase.
+// This allows the defining type to adjust behaviour based on the context of the phase.
+// Here we are reading a yaml file from a directory identified by the name of the phase.
+func (c *AppResource) ReadFrom(_ context.Context, meta core.Metadata, fs fs.Filesystem) error {
+	deployment, err := readDeployment(fs, fmt.Sprintf("env/%s/deployment.yaml", meta.Name))
+	if err != nil {
+		return err
+	}
+
+	if containers := deployment.Spec.Template.Spec.Containers; len(containers) > 0 {
+		ref, err := registry.ParseReference(containers[0].Image)
+		if err != nil {
+			return err
+		}
+
+		digest, err := ref.Digest()
+		if err != nil {
+			return err
+		}
+
+		c.ImageDigest = digest.String()
+	}
+
+	return nil
+}
+
+// WriteTo is a Git specific resource requirement.
+// It specifies how to write the resource to a target Filesystem.
+// The type should navigate and encode the state of the resource to the target Filesystem.
+// The function is also provided with metadata for the calling phase.
+// This allows the defining type to adjust behaviour based on the context of the phase.
+// Here we are writing to a yaml file in a directory identified by the name of the phase.
+func (c *AppResource) WriteTo(ctx context.Context, meta glu.Metadata, fs fs.Filesystem) error {
+	path := fmt.Sprintf("env/%s/deployment.yaml", meta.Name)
+	deployment, err := readDeployment(fs, path)
+	if err != nil {
+		return err
+	}
+
+	if containers := deployment.Spec.Template.Spec.Containers; len(containers) > 0 {
+		containers[0].Image = fmt.Sprintf("%s@%s", c.Image, c.ImageDigest)
+	}
+
+	fi, err := fs.OpenFile(
+		path,
+		os.O_WRONLY|os.O_TRUNC,
+		0644,
+	)
+	if err != nil {
+		return err
+	}
+
+	defer fi.Close()
+
+	data, err := yaml.Marshal(deployment)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(fi, bytes.NewReader(data))
+	return err
+}
+
+func readDeployment(fs fs.Filesystem, path string) (*appsv1.Deployment, error) {
+	fi, err := fs.OpenFile(
+		path,
+		os.O_RDONLY,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer fi.Close()
+
+	deployment := &appsv1.Deployment{}
+	dec := k8syaml.NewYAMLOrJSONDecoder(fi, 1000)
+	if err := dec.Decode(&deployment); err != nil {
+		return nil, err
+	}
+
+	return deployment, nil
+}
